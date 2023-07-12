b4f97777f1ba5392575c58b56a16cecc95e1edef
Tripurari Singh <tripurarisingh1994@gmail.com>
2023-07-12 11:19:37 +0530

    Added the task creation logic

--- backend/routes/taskRoute.ts
+++ backend/routes/taskRoute.ts
@@ -1,10 +1,34 @@
 import express, {Request, Response, Router} from 'express';
 const route: Router = express.Router();
+import pool from '../db';
+import { ResultSetHeader } from 'mysql2';
 
-route.post('/save', (req: Request, res: Response) => {
-    const data = req.body;
-    console.log(data)
-    res.send(data)
+
+route.post('/save', async (req: Request, res: Response) => {
+    try {
+        const {taskName, description, user_id} = req.body;
+
+        const [result] = await pool.execute(
+            'Insert into task (taskName, description, user_id) Values (?,?,?)',
+            [taskName, description, user_id]
+        )
+        
+        if('affectedRows' in result && 'insertId' in result) {
+            if((result as ResultSetHeader).affectedRows === 1) {
+                res.status(201).json({ success: true, message: "inserted successfully"})
+            }
+            else {
+                res.status(200).json({ success: false, message: "insertion failed"})
+            }
+        }        
+
+      } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+      finally {
+        pool.end();
+      }
 })
 
 export default route;
