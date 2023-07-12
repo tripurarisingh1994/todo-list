e9130aecaf909adbd4bfb3417fecddbb09a51226
Tripurari Singh <tripurarisingh1994@gmail.com>
2023-07-12 17:15:43 +0530

    refactor (TaskRoute) : - Added the logic to fetch last inserted data and send to client side - Remove the finally block with pool.end() method due to keep connection alive

--- backend/routes/taskRoute.ts
+++ backend/routes/taskRoute.ts
@@ -1,51 +1,56 @@
-import express, {Request, Response, Router} from 'express';
+import express, { Request, Response, Router } from 'express';
 const route: Router = express.Router();
 import pool from '../db';
 import { ResultSetHeader, RowDataPacket } from 'mysql2';
 
 
 route.post('/', async (req: Request, res: Response) => {
-    try {
-        const {taskName, description, user_id} = req.body;
-
-        const [result] = await pool.execute(
-            'Insert into task (taskName, description, user_id) Values (?,?,?)',
-            [taskName, description, user_id]
-        )
-        
-        if('affectedRows' in result && 'insertId' in result) {
-            if((result as ResultSetHeader).affectedRows === 1) {
-                res.status(201).json({ success: true, message: "inserted successfully"})
-            }
-            else {
-                res.status(200).json({ success: false, message: "insertion failed"})
-            }
-        }        
-
-      } catch (error) {
-        console.error('Error executing query:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      }
-      finally {
-        pool.end();
-      }
-})
-
-route.get('/',async (req: Request, res: Response) => {
-  
   try {
-   const [rows] = await pool.execute('Select * from task');
+    const { taskName, description, user_id = 1 } = req.body;
 
-   const tasks: RowDataPacket[] = rows as RowDataPacket[];
+    const [result] = await pool.execute(
+      'Insert into task (taskName, description, user_id) Values (?,?,?)',
+      [taskName, description, user_id]
+    )
 
-   res.status(200).json({success: true, data: tasks});
-   
+    if ('affectedRows' in result && 'insertId' in result) {
+      if ((result as ResultSetHeader).affectedRows === 1) {
+
+        const insertedTaskId = (result as ResultSetHeader).insertId;
+
+        // Fetch the last inserted task using the insertedTaskId
+        const [rows] = await pool.execute(
+          'SELECT * FROM task WHERE id = ?',
+          [insertedTaskId]
+        );
+
+        const insertedTask: RowDataPacket[] = rows as RowDataPacket[];
+
+        res.status(201).json({ success: true, message: "inserted successfully", tasks: insertedTask })
+      }
+      else {
+        res.status(200).json({ success: false, message: "insertion failed" })
+      }
+    }
+
   } catch (error) {
     console.error('Error executing query:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-  finally {
-    pool.end();
+})
+
+route.get('/', async (req: Request, res: Response) => {
+
+  try {
+    const [rows] = await pool.execute('Select * from task');
+
+    const tasks: RowDataPacket[] = rows as RowDataPacket[];
+
+    res.status(200).json({ success: true, tasks });
+
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
