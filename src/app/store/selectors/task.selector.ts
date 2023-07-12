f693edd3430b368767b4bec69fba5f07de2a9443
Tripurari Singh <tripurarisingh1994@gmail.com>
2023-07-12 17:22:37 +0530

    Refactor (NgRx Store - TaskSelector): Removed the unused code

--- src/app/store/selectors/task.selector.ts
+++ src/app/store/selectors/task.selector.ts
@@ -1,23 +1,5 @@
-// import {AppState, TaskState} from "../app.state";
-// import {createSelector} from "@ngrx/store";
-
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { TaskState } from "../reducers/task.reducer";
-
-
-// export const selectTaskFeature = (state: AppState) => state.task;
-
-
-// export const selectTask = createSelector(
-//   selectTaskFeature,
-//   (state) => {
-//     console.log(state)
-
-//     return {
-//       ...state
-//     }
-//   }
-// )
 
 
 export const selectTaskState = createFeatureSelector<TaskState>('tasks');
