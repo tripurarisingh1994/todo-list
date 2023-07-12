30665cf0f095e3377526c37eb25bbd69ab13388d
Tripurari Singh <tripurarisingh1994@gmail.com>
2023-07-12 17:21:56 +0530

    Refactor + feat ( NgRx  Store - Task Reducer): - Removed unused code - Added the 'on' fun in reducer for getTasks realted

--- src/app/store/reducers/task.reducer.ts
+++ src/app/store/reducers/task.reducer.ts
@@ -1,72 +1,63 @@
-// import {createReducer, on} from "@ngrx/store";
-// import {taskCreate, taskDelete, taskUpdate} from "../actions/task.action";
-
-// export const initialState = {}
-
-// export const taskReducer = createReducer(
-//   initialState,
-//   on(taskCreate, (state,action) => {
-//     return {
-//       ...state,
-//       ...action
-//     }
-//   }),
-//   on(taskUpdate, (state) => {
-
-//     return {
-//       ...state
-//     }
-//   }),
-//   on(taskDelete, (state) => {
-//     return {
-//       ...state
-//     }
-//   })
-// )
-
 import { createReducer, on } from '@ngrx/store';
-import { createTask, createTaskFail, createTaskSuccess } from '../actions/task.action';
+import { createTask, createTaskFail, createTaskSuccess, getTasks, getTasksFailure, getTasksSuccess } from '../actions/task.action';
 
-export interface Task  {
-    taskName: string;
-    description: string;
+export interface Task {
+  id?: number;
+  taskName: string;
+  description: string;
+  user_id?: number;
 }
 
 export interface TaskState {
-    tasks: Task[];
-    loading: boolean;
-    error: string | null;
+  tasks: Task[];
+  loading: boolean;
+  error: string | null;
 }
 
 export const initialState: TaskState = {
-    tasks: [],
-    loading: false,
-    error: null
+  tasks: [],
+  loading: false,
+  error: null
 };
 
 export const taskReducer = createReducer(
   initialState,
-  on(createTask, (state, { taskName, description }) => {
-    // Handle the createTask action
-    // You can update the state, such as setting a loading flag
- 
-    const newTask = [...state.tasks, {taskName, description}]
-    console.log(newTask)
-
-    return { ...state, tasks: newTask /* update the state properties as needed */ };
-  }),
-  on(createTaskSuccess, (state) => {
-    // Handle the createTaskSuccess action
-    // Update the state to reflect the successful task creation
-
-    return { ...state, /* update the state properties as needed */ };
-  }),
-  on(createTaskFail, (state, { error }) => {
-    // Handle the createTaskFail action
-    // Update the state to handle the failure, such as displaying an error message
-
-    return { ...state, /* update the state properties as needed */ };
-  })
+  on(createTask, (state, { taskName, description }) => ({
+    ...state,
+    taskName,
+    description,
+    loading: true,
+    error: null,
+  })),
+  on(createTaskSuccess, (state, { tasks }) => ({
+    ...state,
+    tasks: [...state.tasks, ...tasks],
+    loading: false,
+    error: null,
+  })),
+  on(createTaskFail, (state, { error }) => ({
+    ...state,
+    tasks: [],
+    loading: false,
+    error,
+  })),
+  on(getTasks, (state) => ({
+    ...state,
+    loading: true,
+    error: null,
+  })),
+  on(getTasksSuccess, (state, { tasks }) => ({
+    ...state,
+    tasks,
+    loading: false,
+    error: null,
+  })),
+  on(getTasksFailure, (state, { error }) => ({
+    ...state,
+    tasks: [],
+    loading: false,
+    error,
+  }))
 );
 
 
