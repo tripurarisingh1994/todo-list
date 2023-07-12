a6d88f6e900fdbe988ba628ea7a7316c9e764fd0
Tripurari Singh <tripurarisingh1994@gmail.com>
2023-07-12 17:19:22 +0530

    feat(NgRx Store - TaskEffect): - Added getTask$ effect

--- src/app/store/effects/task.effects.ts
+++ src/app/store/effects/task.effects.ts
@@ -1,24 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
-import { createTask, createTaskFail, createTaskSuccess } from '../actions/task.action';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
+import { createTask, createTaskFail, createTaskSuccess, getTasks, getTasksFailure, getTasksSuccess } from '../actions/task.action';
 import { TaskService } from 'src/app/services/task-service.service';
 
 
 @Injectable()
 export class TaskEffects {
+
   createTask$ = createEffect(() =>
     this.actions$.pipe(
       ofType(createTask),
       mergeMap((action) =>
         this.taskService.createTask(action.taskName, action.description).pipe(
-          map(() => createTaskSuccess()),
+          // tap(res => console.log(res)),
+          map((res:any) => createTaskSuccess(res)),
           catchError((error) => of(createTaskFail({ error: error.message })))
         )
       )
     )
   );
+
+  getTasks$ = createEffect(() =>
+  this.actions$.pipe(
+    ofType(getTasks),
+    mergeMap(() =>
+      this.taskService.getTask().pipe(
+      //  tap(res => console.log(res)),        
+        map((res:any) => getTasksSuccess(res)),
+        catchError((error) => of(getTasksFailure({ error })))
+      )
+    )
+  )
+);
+
 
   constructor(private actions$: Actions, private taskService: TaskService) {}
 }
